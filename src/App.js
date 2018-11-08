cc86a3c578de4b811e1cc2a8de838c17c8c3abbd
Chris Neale <chris@komododigital.co.uk>
2018-11-08 07:53:43 +0000

    Mouse events

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import React, { Component } from 'react';
+import Symbols from './Symbols';
 import './App.css';
 
 class App extends Component {
 
   ref = React.createRef();
-  history = '0,0';
+  history = [];
+  scratching = false;
 
   componentDidMount(){
 
     let Scratchy = this.ref.current;
 
+    Scratchy.addEventListener("mouseenter", e => {
+      this.scratching = false;
+    });
+
+    Scratchy.addEventListener("mouseleave", e => {
+      this.scratching = false;
+    });
+
+    Scratchy.addEventListener("mousedown", e => {
+      this.scratching = true;
+    });
+
+    Scratchy.addEventListener("mouseup", e => {
+      this.scratching = false;
+    });
+
     Scratchy.addEventListener("mousemove", e => {
-      this.history += ","+e.layerX+","+e.layerY;
-      Scratchy.style.setProperty('--paintlet-history', this.history);
+      if (this.scratching) {
+        this.history.push(e.layerX, e.layerY);
+        Scratchy.style.setProperty('--paintlet-history', this.history.join(','));
+      }
     });
 
   }
 
   render() {
     return (
-      <div className="Scratchy" ref={this.ref}>
-        <div className="Symbols">
-          <div><i className="fab fa-aws"></i></div>
-          <div><i className="fab fa-github"></i></div>
-          <div><i className="fab fa-edge"></i></div>
-          <div><i className="fab fa-css3"></i></div>
-          <div><i className="fab fa-chrome"></i></div>
-          <div><i className="fab fa-apple"></i></div>
-          <div><i className="fab fa-html5"></i></div>
-          <div><i className="fab fa-sass"></i></div>
-          <div><i className="fab fa-firefox"></i></div>
-        </div>
-      </div>
+      <main className="Scratchy" ref={this.ref}>
+        <Symbols />
+      </main>
     );
   }
 }
---
+++ src/Symbols.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Symbols extends Component {
+
+  render() {
+    return (
+        <div className="Symbols">
+            <div><i className="fab fa-aws"></i></div>
+            <div><i className="fab fa-github"></i></div>
+            <div><i className="fab fa-edge"></i></div>
+            <div><i className="fab fa-css3"></i></div>
+            <div><i className="fab fa-chrome"></i></div>
+            <div><i className="fab fa-apple"></i></div>
+            <div><i className="fab fa-html5"></i></div>
+            <div><i className="fab fa-sass"></i></div>
+            <div><i className="fab fa-firefox"></i></div>
+        </div>
+    );
+  }
+}
+
+export default Symbols;
