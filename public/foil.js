ac9308f26ff269220cb48d83222e48633108b83b
Chris Neale <chris@komododigital.co.uk>
2018-11-09 13:29:59 +0000

    Experiment with CSS property

--- public/foil.js
+++ public/foil.js
@@ -2,10 +2,34 @@
 
 registerPaint('foil', class FoilPainter {
 
-    static get inputProperties() { return ['--paintlet-history']; }
+    static get inputProperties() { return ['--paintlet-history', '--font-bitmap']; }
 
     paint(ctx, geom, properties) {
 
+        let map = [];
+        let x, y;
+        for (let i=0; i<26; i++) {
+            x = (i%9) * 32;
+            y = Math.floor(i/9) * 32;
+            map[65+i] = [x,y];
+        }
+  
+        for (let i=0; i<10; i++) {
+            x = ((i+26)%9) * 32;
+            y = Math.floor((i+26)/9) * 32;
+            map[48+i] = [x,y];
+        }
+
+        const font = properties.get('--font-bitmap');
+
+        if (font.state==='ready') {
+            let text = "Scratchy";
+            for (let c=0; c<text.length; c++) {
+                let l = text.charCodeAt(c);
+                if (l===32) { continue; }
+                ctx.drawImage(font, map[l][0],map[l][1], 32,32, c*32,0, 32,32);
+            }    
+        }
         const history = properties.get('--paintlet-history').toString().split(",");
 
         ctx.fillStyle = 'rgba(255,255,255,1)';
